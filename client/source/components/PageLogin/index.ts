39091b599f6ed0d44744ff146087bc146ee9c1b9
Nikita Stroganov <findoss@yandex.ru>
2020-07-01 09:52:14 +0300

    страница входа

--- client/source/components/PageLogin/index.ts
+++ client/source/components/PageLogin/index.ts
@@ -2,8 +2,38 @@ import { Component } from '../Component';
 import * as template from './template.hbs';
 import './style.css';
 
+import { Box } from '../Box/index';
+import { Button } from '../Button/index';
+import { Input } from '../Input/index';
+
 export class PageLogin extends Component {
   render(props: any): string {
-    return template(props);
+    const box = new Box();
+
+    const buttonSubmit = new Button().render({
+      text: 'Login',
+      type: 'submit',
+    });
+
+    const inputLogin = new Input().render({
+      labelText: 'Login',
+      type: 'text',
+    });
+
+    const inputPassword = new Input().render({
+      labelText: 'Password',
+      type: 'password',
+    });
+
+    return box.render({
+      slot: template({
+        slots: [inputLogin, inputPassword, buttonSubmit],
+        title: 'Login',
+        link: {
+          text: 'New account',
+          url: './registration',
+        },
+      }),
+    });
   }
 }
