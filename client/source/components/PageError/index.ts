722e22314f609d030094a7d50023e7bceca1ec75
Nikita Stroganov <findoss@yandex.ru>
2020-07-01 09:52:00 +0300

    страница ошибки

--- client/source/components/PageError/index.ts
+++ client/source/components/PageError/index.ts
@@ -2,8 +2,13 @@ import { Component } from '../Component';
 import * as template from './template.hbs';
 import './style.css';
 
+// import * as Handlebars from 'handlebars';
+import { Box } from '../Box/index';
+
 export class PageError extends Component {
   render(props: any): string {
-    return template(props);
+    const box = new Box();
+    const pageError = template(props);
+    return box.render({ slot: pageError });
   }
 }
